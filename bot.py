f8a722ad87ef955feae8924aa292f48c6de1f203
yr4jv4k69p-code <yr4jv4k69p@privaterelay.appleid.com>
2025-12-07 21:35:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,4 +155,15 @@ def main():
 
 if __name__ == "__main__":
     main()
-  
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot çalışıyor", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
